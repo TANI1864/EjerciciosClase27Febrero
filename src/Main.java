6db5a196d8e8e31e14124cd512b46264166030d2
TANI1864 <taniabeltran2020@gmail.com>
2025-02-27 17:11:19 -0500

    Ejercio1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int max = 10;
+        int min = 1;
+        int rondas, d1, d2;
+        int contD1=0, contD2=0;
+        int Dif;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        Random ale = new Random();
+
+
+        rondas = ale.nextInt((max - min +1)) + min;
+        System.out.println("CANTIDAD DE RONDAS: " + rondas);
+                for(int i=1 ; i<=rondas; i++){
+                    d1 = ale.nextInt((6 - 1 + 1)) + 1;
+                    contD1 += d1;
+
+                    d2 = ale.nextInt((6 - 1 + 1)) + 1;
+                    contD2 += d2;
+
+                    System.out.println();
+                    System.out.println("D1: " + d1);
+                    System.out.println("D2: " + d2);
+                }
+        if (contD1 > contD2) {
+            System.out.println("¡El dado 1 ganó!");
+        } else if (contD2> contD1) {
+            System.out.println("¡El dado 2 ganó!");
+       } else {
+            System.out.println("¡Es un empate!");
         }
     }
+
+
 }
